27889722c160f1d630b0dc9e1acefc7ecaccf8e6
Mohammad Ayub <mohammad.ayub@imagination.com>
2021-01-06 13:20:26 +0000

    added ajax call

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -4,9 +4,14 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
+from django.http import HttpResponse
+from django.core.paginator import Paginator
+from django.core.paginator import EmptyPage
+from django.core.paginator import PageNotAnInteger
 
 from .forms import ImageCreateForm
 from .models import Image
+from bookmarks.common.decorators import ajax_required
 
 
 @login_required
@@ -41,6 +46,7 @@ def image_detail(request, id, slug):
                    'image': image})
 
 
+@ajax_required
 @login_required
 @require_POST
 def image_like(request):
@@ -57,3 +63,29 @@ def image_like(request):
         except:
             pass
     return JsonResponse({'status': 'error'})
+
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        images = paginator.page(1)
+    except EmptyPage:
+        if request.is_ajax():
+            # If the request is AJAX and the page is out of range
+            # return an empty page.
+            return HttpResponse('')
+        # If page is out of range deliver last page of results
+        images = paginator.page(paginator.num_pages)
+    if request.is_ajax():
+        return render(request,
+                      'images/image/list_ajax.html',
+                      {'selection': 'images', 'images': 'images'})
+    return render(request,
+                  'images/image/list.html',
+                  {'section': 'images', 'images': 'images'})
